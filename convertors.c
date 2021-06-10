bb525219e98da30e6a269ee74b4f6f9ea7f9e08d
Alita Mohiam <amohiam@oa-j5.msk.21-school.ru>
2021-06-10 12:44:57 +0300

    why is there a floating point error

--- convertors.c
+++ convertors.c
@@ -1,4 +1,6 @@
-#include "ft_printf.h"
+//#include "ft_printf.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int		power(int n, int p)
 {
@@ -24,19 +26,33 @@ int		count_digits(int n, int sys)
 	return (r);
 }
 
-char	*dec_to_hex(int n)
+char	num_to_ch(int n)
+{
+	if (n >= 0 && n <= 15)
+		return ("0123456789abcdef"[n]);
+	return ('!');
+}
+
+char	*conv_from_dec(int n, int sys)
 {
 	char	*res;
 	int		i;
 	int		temp;
 	
-	i = count_digits(n, 16);
+	i = count_digits(n, sys);
 	res = (char *)malloc(i + 1);
 	temp = n;
 	res[i] = 0;
-	while (i > 0)
+	while (--i >= 0)
 	{
-		
+		res[i] = num_to_ch(temp % sys);
+		temp /= sys;
 	}
-	
+	return (res);
+}
+
+int main(void)
+{
+	printf("%c\n", num_to_ch(110));
+	return (0);
 }
